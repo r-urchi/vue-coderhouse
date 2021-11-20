66a6c45ecd4cbc28a2061f1434178f076f4e5987
Ramon Urchipia <ramonurchipia@gmail.com>
2021-11-20 11:30:51 -0300

    tabla desafio 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
+Vue.component('table-products', {
+    props:['products'],
+    template: `
+        <table class="table">
+            <tr class="table__tr">
+                <th class="table__th">N°</th>
+                <th class="table__th">Producto</th>
+                <th class="table__th">Tipo</th>
+            </tr>
+            <tr v-for="product in products">
+                <td>{{product.id}}</td>
+                <td>{{product.product}}</td>
+                <td>{{product.type}}</td>
+            </tr>
+        </table>
+    `,
+})
+
 var app = new Vue({
     el: '#app',
     data: {
       counter: 0,
+      mode: false,
+      products: [
+        {
+          id: 1,
+          product: "Guitarra",
+          type: "Les Paul",
+        },
+        {
+          id: 2,
+          product: "Bajo",
+          type: "Jazzbass",
+        },
+        {
+          id: 3,
+          product: "Guitarra acústica",
+          type: "Classic",
+        },
+        {
+          id: 4,
+          product: "Bajo acústico",
+          type: "Classic",
+        },
+        {
+          id: 5,
+          product: "Guitarra",
+          type: "Stratocaster",
+        },
+      ],
     },
     methods: {
         add(){
@@ -10,10 +56,16 @@ var app = new Vue({
         subtract(){
             this.counter === 0 ? this.counter = 0 : this.counter--;
         },
+        darkMode(){
+            this.mode = !this.mode;
+        }
     },
     computed: {
         counterValue(){
-            return this.counter
+            return this.counter;
         },
+        changeMode(){
+            return this.mode ? 'Modo claro' : 'Modo oscuro';
+        }
     },
 });
